c6b1ae6b5abe351a58bd2334f204690a1e271bfb
Danny Sun <danny.sun@companiondxlab.com>
2016-05-21 01:21:26 -0500

    fixed listitem key

--- public/js/main.js
+++ public/js/main.js
@@ -19674,7 +19674,7 @@ module.exports = require('./lib/React');
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredients = [{ id: 1, text: "ham" }, { id: 1, text: "cheese" }, { id: 1, text: "bacon" }];
+var ingredients = [{ id: 1, text: "ham" }, { id: 2, text: "cheese" }, { id: 3, text: "bacon" }];
 
 var List = React.createClass({
   displayName: 'List',
